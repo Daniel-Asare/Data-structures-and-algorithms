c0a9e742ad7c1d6335162298ee7cf3f6a3e55549
Daniel Asare <dannyasare29@gmail.com>
2024-04-22 13:24:21 +0000

    first commit

---
+++ arrays.js
@@ -0,0 +1,88 @@
+/*
+const numbers = [1,2,4,8];
+console.log(numbers);numbers
+
+numbers[4] = 16;
+console.log(numbers);
+
+numbers.push(12,20);
+console.log(numbers);
+
+numbers.unshift(0);
+console.log(numbers);
+
+const lastNumber = numbers.pop();
+console.log(lastNumber);
+console.log(numbers.shift());
+console.log(numbers);
+
+numbers.splice(2,0,6);
+console.log(numbers);
+
+const newNumbers = numbers.slice(0,3)
+console.log(newNumbers);
+
+// const list = ['milk','eggs','butter','bread','bacon','sausage','juice'];
+
+const list = 
+[
+  {text:"milk", cost:4, need:false},
+  {text:"eggs", cost:19, need:true},
+  {text:"butter", cost:5, need:false},
+  {text:"bread", cost:3, need:true},
+  {text:"bacon", cost:12, need:true}
+]
+
+let html = '';
+let total = 0;
+
+for(let index = 0; index < list.length; index++ ){
+  let items = list[index];
+  if (toyy)
+  html += `<li> ${items.text} </li>`;
+total += items.cost
+}
+
+document.getElementById('list').innerHTML = html
+document.getElementById('cost').innerHTML = `$${total}`;
+
+const names = ['John','daniel','Kudus','barry','Ben',1,2,3];
+
+console.log(names.length);
+console.log(names[names.length - 1]);
+
+// concat
+
+const lastNames = ['Pepper','Banana', 'Onion'];
+const allNames = names.concat(lastNames)
+console.log(allNames);
+
+
+const names = ['Anna','Susy','Bob'];
+const lastName = 'Shakeandbake';
+
+let newArray = [];
+
+for (let i = 0; i < names.length; i++) {
+  const fullName = ` ${names[i]} ${lastName}`;
+  //  console.log(fullName);
+   newArray.push(fullName);
+}
+console.log(newArray);
+
+1 > 0 ? console.log('Value is true') : console.log('Value is false');
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ loops.js
@@ -0,0 +1,16 @@
+// for (let i = 5; i >= 1; i--){
+//   if (i % 2 !== 0){
+//     console.log(i);
+//   }
+// }
+
+for(let i = 1; i <= 20; i++){
+  if(i === 13){
+    break;
+  } else{
+    console.log(i);
+  }
+}
+
+let logIn = false;
+let username;
