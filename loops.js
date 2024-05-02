34bdf6770f892ed266a0e0ea0752c0b9214a80b1
Daniel Asare <dannyasare29@gmail.com>
2024-05-02 10:58:11 +0000

    Updated

--- loops.js
+++ loops.js
@@ -74,3 +74,12 @@ console.log(ages);
     rep++;
   }
 
+  let dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  while(dice === 6){
+    console.log(`you roll six`);
+    dice++;
+  }
+
+
+  
