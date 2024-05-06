64da0ee997f876f72fe1f93e863bdf284abd680c
Daniel Asare <dannyasare29@gmail.com>
2024-05-06 15:36:43 +0000

    Updated

--- objects.js
+++ objects.js
@@ -180,3 +180,8 @@ console.log('' || 'Daniel');
 console.log(true || 0);
 console.log(undefined || null);
 console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+const guest1 = restaurant.numGuests ? restaurant.numGuests: 10;
+console.log(guest1);
+
+const guest2  = restaurant.numGuests || 10;
