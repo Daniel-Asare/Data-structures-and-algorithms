43efbfae572c7078e1a858b8e6c2549d55bb0a6f
Daniel Asare <dannyasare29@gmail.com>
2024-05-08 17:45:19 +0000

    Updated

---
+++ powersOfTwo.js
@@ -0,0 +1,13 @@
+
+
+
+// Give a postive interger 'n' determine if the number is a power of 2 or not. An integer is a power of two if there exist an integer 'x' such that 'n' ===2
+
+function powerOfTwo(n){
+  if(n < 1){
+    return false;
+  }
+  for(let i = 1; i <= n; i++){
+    
+  }
+}
