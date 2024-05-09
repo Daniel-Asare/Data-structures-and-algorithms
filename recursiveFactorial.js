038998b6c9bdeefcce0e94c193d05df4cc84b596
Daniel Asare <dannyasare29@gmail.com>
2024-05-09 10:14:15 +0000

    Updated

--- recursiveFactorial.js
+++ recursiveFactorial.js
@@ -1,1 +1,14 @@
 // Giving an integer 'n', find the factorial of that integer
+
+function recursiveFactorial(n){
+  if(n < 2){
+    return 1;
+  }
+  return n * (recursiveFactorial(n-1))
+}
+
+console.log(recursiveFactorial(1));
+console.log(recursiveFactorial(5));
+console.log(recursiveFactorial(6));
+
+// Big O - O(n)
